6154aa78317e07a60b675330f204977179ffd05a
Kunal <kunaljain1510@gmail.com>
2023-03-22 00:49:06 +0530

    created different options for User to selct from

---
+++ src/AIOptions/index.jsx
@@ -0,0 +1,37 @@
+const arrayItems = [{
+    name: 'Q&A',
+    id: 'q&a',
+    description: "Answer Question based on existing knowledge",
+},
+{
+    name: "Gramer Correction",
+    id: "graeerCorrection",
+    description: "corrects sentences into standard English",
+},
+{
+    name: "Sunnarize for a 2nd grader",
+    id: "summary",
+    description: "Translates difficult text into simpler concepts",
+},
+{
+    name: "English to other langvages",
+    id: "translate",
+    description: "Translates English text into French, Spanish and japense",
+},
+{
+    name:"Text to Command",
+    id:"Translate text into programming commands",
+    description:"Translate text into programming commands",
+},
+{
+    name:"Explain code",
+    id: "explaincode",
+    description:"Explain complicated piece of code.",
+},
+{
+    name:"Calculate time Complexity",
+    id:"calculatetime",
+    description:"Find time complexity of a function."
+}
+];
+
---
+++ src/component/OptionSelection.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function OptionSelection(){
+    return (<>
+    <h1 className="heading">React AI App</h1>
+    </>
+    );
+}
