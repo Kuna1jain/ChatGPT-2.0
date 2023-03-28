e21ca2532e63eb543c6bbbdeca7a9b0342f830bf
Kunal <kunaljain1510@gmail.com>
2023-03-28 16:11:51 +0530

    created interactive textarea

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Translation from './component/Translation';
 import {arrayItems} from "./AIOptions";
 function App() {
    const[option,setOption]=useState({});
+   const [input,setInput] = useState({});
 //  console.log(import.meta.env.VITE_OPEN_AI_KEY);
 //  console.log(arrayItems);
-const SelectOption=(option)=>{
+const SelectOption= (option) =>{
    setOption(option);
 };
-console.log(Object.values(option).length); //converts it into an array of values
+const doStuff=()=>{};
+console.log(input); //converts it into an array of values
  return (<div className='App'>
     
    { Object.values(option).length===0?(
    <OptionSelection arrayItems={arrayItems}SelectOption={SelectOption}/>)
-    :(<Translation/>)}
+    :(<Translation doStuff={doStuff} setInput={setInput}/>)}
  </div>
  );
 }
--- src/component/Translation.jsx
+++ src/component/Translation.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-export default function Translation() {
+export default function Translation({doStuff,setInput}) {
     return(
         <div>
            <p>Enter your Prompt</p> 
-            <textarea className="text-area" id="ta" cols="50" rows="12"></textarea>
+            <textarea 
+            className="text-area" 
+            id="ta" 
+            cols="50"
+            rows="12" 
+            onChange={(e) => setInput(e.target.value)} 
+            ></textarea>
+            <button className='action-btn' onClick={doStuff}>Proceed</button>
+            {/* <button>Reset</button> */}
         </div>
     )
 };
